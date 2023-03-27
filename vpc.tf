1f57875c1d31248c1b1a4b3e8de207980eabfcc2
nelly123her <nelly12345gok@gmail.com>
2023-03-26 22:51:23 -0600

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,18 @@
 module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "3.4.0"
-  subnets = [
-    {
-      subnet_name   = "gaurav-subnet"
-      subnet_ip     = "10.100.10.0/24"
-      subnet_region = var.region
-    }
-  ]
+    source  = "terraform-google-modules/network/google"
+    version = "3.4.0"
 
+    project_id   = "p-g3wzcbp6hbrn-0"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+   subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+
+   
 }
