16f4a4a7d28aa5b2b5296003da757949c16e7929
JorgeP7 <36684739+JorgeP7@users.noreply.github.com>
2023-03-29 21:14:20 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,15 @@
-module "vpc" {
- source  = "app.terraform.io/PalmaInc/network/google"
-  version = "3.4.0"
+#module "vpc" {
+# source  = "app.terraform.io/PalmaInc/network/google"
+#  version = "3.4.0"
 
-    project_id   = "p-uhvybumfpbw9-0"
-    network_name = "gaurav-network"
+#    project_id   = "p-uhvybumfpbw9-0"
+#    network_name = "gaurav-network"
 
-   subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]  
-}
+#   subnets = [
+#  {
+#    subnet_name   = "gaurav-subnet"
+#    subnet_ip     = "10.100.10.0/24"
+#    subnet_region = var.region
+#  }
+#]  
+#}
