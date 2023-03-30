e54dcd151872ad9d4bb065e6e04494d768f53b52
JorgeP7 <36684739+JorgeP7@users.noreply.github.com>
2023-03-29 21:18:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -124,19 +124,3 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
-
-module "vpc" {
- source  = "app.terraform.io/PalmaInc/network/google"
-  version = "3.4.0"
-
-    project_id   = "p-uhvybumfpbw9-0"
-    network_name = "gaurav-network"
-
-   subnets = [
-    {
-      subnet_name   = "gaurav-subnet"
-      subnet_ip     = "10.100.10.0/24"
-      subnet_region = var.region
-    }
-  ]  
-}
